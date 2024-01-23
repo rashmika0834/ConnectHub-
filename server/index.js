f6d04f4af4f166d9d9e3636e33ef33b4b9e7e200
RashmikaE <rashmikapramodya@gmail.com>
2024-01-23 17:41:08 +0530

    Mongoose Setup

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { error } from "console";
 
 
 /* configurations*/
@@ -38,3 +39,13 @@ const storage = multer.diskStorage({
 });
  
 const upload = multer({storage});
+
+
+/*MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`));
