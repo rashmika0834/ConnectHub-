07e508949be5bdac40b27d512fea2d307c3e3207
RashmikaE <rashmikapramodya@gmail.com>
2024-01-26 00:59:41 +0530

    schema created for a new user

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-
+import { register } from "./controllers/auth.js";
 
 /* configurations*/
 
@@ -40,7 +40,8 @@ const storage = multer.diskStorage({
 const upload = multer({storage});
 
 /* ROUTES WITH FILES */
-// app.post("/auth/register", upload.single("picture"), register);
+app.post("/auth/register", upload.single("picture"), register);
+
 
 /*MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
