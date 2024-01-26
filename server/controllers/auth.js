e96a8537fef48375f71192413d0af46cb2fdcf55
RashmikaE <rashmikapramodya@gmail.com>
2024-01-27 00:05:24 +0530

    exception added

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -35,7 +35,6 @@ export const register = async(req, res) => {
         const savedUser = await newUser.save();
         res.status(201).json(savedUser);
     } catch (err) {
-
+        res.status(500).json({error: err.message });
     }
- 
 }
